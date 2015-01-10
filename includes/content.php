c88e70fbe8e721576f13ff058d4d7bd34e471ecd
Rev Dave Wakely <dave@gurucomputers.co.uk>
2015-01-10 21:27:47 +0000

    First Commit for Skeleton

---
+++ includes/content.php
@@ -0,0 +1,5 @@
+<?php
+
+if($page == 'freehost') {
+	include 'freehost.php'
+}
---
+++ includes/head.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="description" content="Need a professional Webhost? Let us do I.T.">
+		<meta name="keywords" content="YouCentral"/>
+		<meta property="og:title" content='Guru Web Services'/>
+		<meta property="og:type" content="article"/>
+		<meta property="og:image" content="./img/logo.png"/>
+		<meta property="og:description" content="Need a professional Webhost? Let us do I.T."/>
+		<meta name="twitter:card" content="summary"/>
+		<meta name="twitter:title" content='Guru Web Services'/>
+		<meta name="twitter:description" content="Need a professional Webhost? Let us do I.T."/>
+		<meta name="twitter:image" content="./img/logo.png"/>
+		<title>
+			Guru Web Services - Guru Computers Ltd
+		</title>
+		<link media="all" type="text/css" rel="stylesheet" href="../css/normalize.css">
+		<link media="all" type="text/css" rel="stylesheet" href="../css/main.css">
+		<link href="../bootstrap/dist/css/bootstrap.min.css" rel="stylesheet" media="screen">
+		<link href='http://fonts.googleapis.com/css?family=Black+Ops+One' rel='stylesheet' type='text/css'>
+		<!--[if lt IE 9]>
+			<script src="bootstrap/assets/js/html5shiv.js"></script>
+			<script src="bootstrap/assets/js/respond.min.js"></script>
+		<![endif]-->
+	</head>
